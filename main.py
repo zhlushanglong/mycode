4f5b5fa554f3660249e86085e4736344ec5c5d4c
zhlushanglong <23223612@qq.com>
2022-11-27 10:29:04 +0800

    003

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def print_hi(name):
 def print_abc():
     print('abc')
 
+def print_efg():
+    print("efg")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
