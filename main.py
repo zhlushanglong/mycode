a752cd9311d660666af803853bc8913a846042b9
zhlushanglong <23223612@qq.com>
2022-11-27 08:42:06 +0800

    001

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm Google Chrome Canary')
+    print_hi('PyCharm Google Chrome Canary. 20221127.001')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
